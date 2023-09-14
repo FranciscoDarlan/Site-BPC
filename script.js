3c5aa3a0da75cbe251eed2d32bb7885cf8005368
FranciscoDarlan <darlann_c_jpa@hotmail.com>
2023-09-13 21:47:08 -0300

    upload name script

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ var menu = document.querySelector('nav ul');
 var menuBar = document.querySelector('nav .menu-icon');
 var iconMenu = document.querySelector('nav .menu-icon img');
 
-menuBar.addEventListener('click', function(){
+function abrirMenu() {
     
-    menu.classList.toggle('active');
-});
+    
+}
+
+menuBar.addEventListener(onclick, abrirMenu)
