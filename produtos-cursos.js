09c82adf903636173ada6e3e6afa84cf354a5dfc
FranciscoDarlan <darlann_c_jpa@hotmail.com>
2024-05-25 18:07:15 -0300

    ARQUIVO DE PREÇO IMG

---
+++ produtos-cursos.js
@@ -0,0 +1,8 @@
+const menuOptions = [
+    { name: 'Bombeiro Civil', price: 1.300, vegan: false, src: './jpeg' },
+    { name: 'APH Avançado', price: 800, vegan: false, src: './png' },
+    { name: 'NR 33', price: 150, vegan: false, src: './png' },
+    { name: 'NR 35', price: 50, vegan: false, src: './png' },
+    { name: 'Stop The Bleed', price: 80, vegan: true, src: './png' },
+    { name: 'Grandes Eventos', price: 50, vegan: true, src: './png' },
+    ]
