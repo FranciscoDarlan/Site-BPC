067c780366d2e5ee2974567d08cc5d5b77503204
FranciscoDarlan <darlann_c_jpa@hotmail.com>
2024-05-27 20:12:38 -0300

    linkando e adincoiopnapod a funcrtion redirecionamento para pagina de curso

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ var menuBar = document.querySelector('nav .menu-icon');
 var iconMenu = document.querySelector('nav .menu-icon img');
 var linkSimulador = document.querySelector('classeSimulador');
 
+let linkIndexCurso = document.querySelector('.linkCurso');
 
+function paginaDeCurso() {
+    window.location.href = "index-cursos.html";
+}
 
 menuBar.addEventListener('click', function () {
     menu.classList.toggle('active');
