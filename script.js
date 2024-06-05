d42fb74485684285bd49f3b4e7681abe0e63d588
FranciscoDarlan <darlann_c_jpa@hotmail.com>
2024-06-05 17:37:53 -0300

    tentando acrescentar o css radios

--- script.js
+++ script.js
@@ -12,3 +12,21 @@ function paginaDeCurso() {
 menuBar.addEventListener('click', function () {
     menu.classList.toggle('active');
 });
+
+
+// TROCA DAS IMAGENS CURSOS
+let count = 1;
+document.getElementById('radio1').checked = true;
+
+setInterval( function() {
+    nextImage();
+}, 1000);
+
+function nextImage() {
+    count++;
+    if (count > 4) {
+        count = 1;
+    }
+    document.getElementById("radio"+count).checked = true;
+}
+
