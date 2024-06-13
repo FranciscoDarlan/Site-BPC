b16c819117ea43cb88eddeba6a95ddf74dd2a76f
FranciscoDarlan <darlann_c_jpa@hotmail.com>
2024-06-13 17:25:05 -0300

    menu responsivo

--- script.js
+++ script.js
@@ -16,3 +16,22 @@ function nextImage() { // função proxima imagem
     document.getElementById("radio" + count).checked = true;
 }
 
+// function que aparece e desapace menu
+function imgTraco() {
+    let ulMenu = document.querySelector('.ul-menu-responsivo')
+    let icon = document.querySelector('.image-icon-traco') 
+
+    console.log(icon)
+    if (ulMenu.classList.contains('remove')){
+        ulMenu.classList.remove('remove');
+        icon.innerHTML = ' <div class="image-icon-traco"><img class="icon" onclick="imgTraco()" src="./asset-img-cursos/img-fecha-x.png" alt="imagem-menu"></div>'
+    }  else{
+        ulMenu.classList.add('remove');
+        // image.src = "./asset-img-cursos/img-fecha-x.png"
+        icon.innerHTML = ' <div class="image-icon-traco"><img class="icon" onclick="imgTraco()" src="./asset-img/menu-removebg-preview.png" alt="imagem-menu"></div>'
+
+
+    }
+    
+    
+}
